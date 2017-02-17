b424a5a81df0d3e21f4572be782e04e1f4cd6561
Miguel Bustamante <phatbastard81@yahoo.com>
2017-02-17 07:57:19 +0000

    all only @place.user to change place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
     
-    before_action :authenticate_user! , only: [:new, :create, :edit]
+    before_action :authenticate_user! , only: [:new, :create, :edit, :update]
 
     def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -22,17 +22,26 @@ class PlacesController < ApplicationController
     
     def edit
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+        return render text: 'Not allowed', status: :forbidden
+        end
     end
     
     def update
         @place = Place.find(params[:id])
+        
+        if current_user != current_user
+         return render text: 'Not Allowed', status: :forbidden
+        end
+     
         @place.update_attributes(place_params)
         redirect_to root_path
     end
     
     def destroy
     @place = Place.find(params[:id])
-    @place = Place.destroy
+    @place.destroy
     redirect_to root_path
     end
     
