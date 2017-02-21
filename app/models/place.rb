908a6d47c6447e3797ae8496bd36002e58257f55
Miguel Bustamante <phatbastard81@yahoo.com>
2017-02-21 03:33:00 +0000

    added geocoder and figaro gems

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,11 @@
 class Place < ApplicationRecord
     
     belongs_to :user
-    validates :name, presence: true
+    geocoded_by :address
+    after_validation :geocode
+    
+    validates :name, :length => {minimum: 4} , presence: true
+    validates :address, presence: true
+    validates :description, presence: true
+    #added for lesson 28
 end
