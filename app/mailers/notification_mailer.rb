4ab12981ee67a071cee5035c0cd5107d25eea7df
Miguel Bustamante <phatbastard81@yahoo.com>
2017-02-28 01:57:21 +0000

    added link to mailer and automated user and place

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ApplicationMailer
     default from: 'no-reply@nomsterapp.com'
     
-    def comment_added
-        mail(to: "phatbastard81@yahoo.com", subject: "You have recieved a comment to your post")
+    def comment_added(comment)
+        @place = comment.place
+        @place_owner = @place.user
+        mail(to: @place_owner.email, 
+        subject: "You have recieved a comment to #{@place.name}")
     end
 end
